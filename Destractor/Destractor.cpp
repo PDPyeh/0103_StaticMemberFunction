6359938942fbc4a428126293390e54063ba24f27
Pradipa Y <merakayamsstraat@gmail.com>
2024-05-13 16:26:31 +0700

    destractor 1

--- Destractor/Destractor.cpp
+++ Destractor/Destractor.cpp
@@ -7,13 +7,22 @@ private:
 	int *arr;
 	string panjang;
 public:
-	angka(int 1) {
+	angka(int i) {
 		panjang = i;
 		arr = new int[i];
-		isiData()
+		isiData();
 	}
-	~angka();
-	void cetakData();
+	~angka() {
+		cout << endl;
+		cetakData();
+		delete[]arr;
+		cout << "Alamat Array Sudah Dilepaskan" << endl;
+
+	}
+
+	void cetakData() {
+
+	}
 	void isiData();
 	
 };
