f7352ca646deaee1da8aae4c783ac19ea8599a2b
Pradipa Y <merakayamsstraat@gmail.com>
2024-05-13 11:10:32 +0700

    membuat integer main di static function

--- StaticFunction/StaticFunction.cpp
+++ StaticFunction/StaticFunction.cpp
@@ -31,5 +31,17 @@ void mahasiswa::printAll() {
 	cout << endl;
 }
 
+int main() {
+	mahasiswa mhs1("Pradipa Yogananda");
+	mahasiswa mhs2("Imron");
+	mahasiswa::setNim(9);
+	mahasiswa mhs3("Lalu");
+	mahasiswa mhs4("Kratos");
 
+	mhs1.printAll();
+	mhs2.printAll();
+	mhs3.printAll();
+	mhs4.printAll();
+
+	cout << "akses dari luar object = " << mahasiswa::getNim() << endl;
 }
