0d18578fdcec9dcb5da7272eb51bf753c7f13ee9
Pradipa Y <merakayamsstraat@gmail.com>
2024-05-13 11:19:26 +0700

    membuat member class mahasiswa

--- constructor/constructor.cpp
+++ constructor/constructor.cpp
@@ -3,5 +3,17 @@
 using namespace std;
 
 class mahasiswa {
+public:
+	int nim;
+	string nama;
 
+public:
+	mahasiswa() {
+		nim = 0;
+		nama = "";
+	};
+	mahasiswa(int);
+	mahasiswa(string);
+	mahasiswa(int iNim, string iNama);
+	void cetak();
 };
