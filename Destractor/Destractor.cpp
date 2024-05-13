580dc3533251d4c18f81cc3545d661cc7c4bf97b
Pradipa Y <merakayamsstraat@gmail.com>
2024-05-13 16:20:01 +0700

    constructtor 1

--- Destractor/Destractor.cpp
+++ Destractor/Destractor.cpp
@@ -7,6 +7,13 @@ private:
 	int *arr;
 	string panjang;
 public:
-	angka(int);
+	angka(int 1) {
+		panjang = i;
+		arr = new int[i];
+		isiData()
+	}
+	~angka();
+	void cetakData();
+	void isiData();
 	
 };
